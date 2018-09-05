809b6a3d7883c92e8bfb577f0876ce0345ada8bf
Jakub Furman <qba@qba.me>
2018-09-05 21:22:03 +0200

    Add plugin auto updates and some code cleanup

--- octoprint_m73etaoverride/__init__.py
+++ octoprint_m73etaoverride/__init__.py
@@ -5,14 +5,29 @@ from octoprint.printer.estimation import PrintTimeEstimator
 m73time = 1
 
 class M73ETA(octoprint.plugin.OctoPrintPlugin):
-  def sent_m73(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+  def handle_m73(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
     global m73time
     if gcode and gcode == "M73":
       m = re.search('(?<=R)\w+', cmd)
       if m:
         m73time = m.group(0)
 
-class CustomPrintTimeEstimator(PrintTimeEstimator):
+  def get_update_information(*args, **kwargs):
+    return dict(
+        updateplugindemo=dict(
+            displayName=self._plugin_name,
+            displayVersion=self._plugin_version,
+
+            type="github_release",
+            current=self._plugin_version,
+            user="sysadminsh",
+            repo="OctoPrint-M73ETAOverride",
+
+            pip="https://github.com/sysadminsh/OctoPrint-M73ETAOverride/archive/{target}.zip"
+        )
+    )
+
+class M73PrintTimeEstimator(PrintTimeEstimator):
   def __init__(self, job_type):
     pass
 
@@ -21,8 +36,8 @@ class CustomPrintTimeEstimator(PrintTimeEstimator):
     estimates = 60 * int(m73time)
     return estimates, "estimate"
 
-def create_estimator_factory(*args, **kwargs):
-    return CustomPrintTimeEstimator
+def m73_create_estimator_factory(*args, **kwargs):
+    return M73PrintTimeEstimator
 
 def __plugin_load__():
   global __plugin_implementation__
@@ -30,6 +45,7 @@ def __plugin_load__():
 
   global __plugin_hooks__
   __plugin_hooks__ = {
-    "octoprint.comm.protocol.gcode.sent": __plugin_implementation__.sent_m73,
-    "octoprint.printer.estimation.factory": create_estimator_factory
+    "octoprint.comm.protocol.gcode.sent": __plugin_implementation__.handle_m73,
+    "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+    "octoprint.printer.estimation.factory": m73_create_estimator_factory
   }
