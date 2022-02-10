6a4fcd6f51b1d954922db9ea3423f07a02a281ff
jonnieZG <jonnieZG@users.noreply.github.com>
2022-02-10 11:54:45 +0100

    Update setup.py
    Updated plugin URL

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Jakub Furman, Arsen Torbarina"
 plugin_author_email = "kontakt@sysadmin.sh"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/sysadminsh/OctoPrint-M73ETAOverride"
+plugin_url = "https://github.com/jonnieZG/OctoPrint-M73ETAOverride"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
